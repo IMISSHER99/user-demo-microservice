9ee7f47d65d7b10acf0e8b0cfcc9cfe6814118f5
imissher <radiantimissher@gmail.com>
2024-07-11 19:51:20 +0530

    UPDATED CREDENTIALS VARIABLE

--- terraform/infrastructure.tf
+++ terraform/infrastructure.tf
@@ -10,7 +10,7 @@ terraform {
 provider "google" {
   project = var.project_id
   region = var.project_region
-  credentials = var.
+  credentials = var.GOOGLE_CREDENTIALS
 }
 
 resource "google_sql_database_instance" "postgres-database-instance" {
