f646c0d4bed233ac38da8d9960340a7454161187
imissher <radiantimissher@gmail.com>
2024-07-11 23:21:40 +0530

    CHANGED FILE NAME

--- terraform/infrastructure.tf
+++ terraform/infrastructure.tf
@@ -16,7 +16,7 @@ terraform {
 provider "google" {
   project = var.project_id
   region = var.project_region
-  credentials = var.GOOGLE_KEY_FILE
+  credentials = var.GOOGLE_APPLICATION_CREDENTIALS
 }
 
 resource "google_sql_database_instance" "postgres-database-instance" {
