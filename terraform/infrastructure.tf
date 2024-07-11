542c4b4de1d1dda887656cd6084fc7bc570d8d6d
Amith <appusai16@gmail.com>
2024-07-11 21:07:16 +0530

    Develop (#19)
    * CREATE DEMO USER MICROSERVICE, NEED TO TEST DOCKER BUILD
    
    * CREATED TERRAFORM INFRASTRUCTURE FOR POSTGRES DATABASE USING CLOUD SQL
    
    * CREATED TERRAFORM INFRASTRUCTURE FOR POSTGRES DATABASE USING CLOUD SQL
    
    * CREATED ACTION TO EXECUTE TERRAFORM
    
    * CREATED ACTION TO EXECUTE TERRAFORM
    
    * ADDED TERRAFORM PLAN STEP
    
    * UPDATED ENV VARIBALE NAME:
    
    * ADDED ENV VARIBALE TO ACTIONS
    
    * CHANGED USERNAME AND PASSWORD VARIABLE
    
    * CHANGED ENV NAME FOR GCP TOKEN
    
    * ADDED STEP TO WRITE CONTENTS OF SECRET TO JSON FILE
    
    * REVERTED YAML
    
    * ADDED STEP TO CREATE A JSON FILE FOR THE TOKEN
    
    * ADDED ENV FOR SERVICE ACCOUNT TOKEN
    
    * UPDATED CREDENTIALS VARIABLE
    
    * UPDATED CREDENTIALS VARIABLE
    
    * ADDED TERRAFORM APPLY STEP (#13)
    
    * Feature (#17)
    
    * ADDED TERRAFORM APPLY STEP
    
    * ADDED CONDITIONAL RUN TO CHECK IF THE INFRASTRUCTURE IS ALREADY CREATED
    
    * ADDED TERRAFORM DESTROY STEP
    
    * ADDED TERRAFORM APPLY
    
    * Feature (#18)
    
    * ADDED TERRAFORM APPLY STEP
    
    * ADDED CONDITIONAL RUN TO CHECK IF THE INFRASTRUCTURE IS ALREADY CREATED
    
    * ADDED TERRAFORM DESTROY STEP
    
    * ADDED TERRAFORM APPLY
    
    * REMOVED AUTOMATIC RUN ON PULL AND PUSH
    
    ---------
    
    Co-authored-by: imissher <radiantimissher@gmail.com>
    Co-authored-by: IMISSHER99 <145341240+IMISSHER99@users.noreply.github.com>

---
+++ terraform/infrastructure.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.37.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region = var.project_region
+  credentials = var.GOOGLE_CREDENTIALS
+}
+
+resource "google_sql_database_instance" "postgres-database-instance" {
+  database_version = var.database_type
+  name = var.database_instance_name
+  region = var.project_region
+
+  settings {
+    tier = var.machine_type
+    availability_type = var.availability_type
+    edition = var.edition
+    user_labels = {
+      environment: var.environment
+    }
+
+  }
+
+  deletion_protection = false
+}
+
+
+resource "google_sql_user" "users" {
+  instance = google_sql_database_instance.postgres-database-instance.name
+  name     = var.USER_NAME
+  password = var.PASSWORD
+}
+
+resource "google_sql_database" "database" {
+  instance = google_sql_database_instance.postgres-database-instance.name
+  name     = var.database_name
+}
